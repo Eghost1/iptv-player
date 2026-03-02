34eb708458fc2f3e8df1a5b6715ee7bd489af371
Eghost1 <diego.bravov@usm.cl>
2026-03-02 20:14:41 -0300

    feat: Configure Vite base path and add gh-pages for GitHub Pages deployment, generating initial build output.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    // Esto hace que las rutas a los archivos CSS y JS sean relativas al index.html
-    // Esencial para que funcione correctamente en GitHub Pages u otros subdirectorios.
-    base: './',
+    // Vite necesita saber la subcarpeta exacta de GitHub pages para cargar los assets.
+    base: '/iptv-player/',
 });
